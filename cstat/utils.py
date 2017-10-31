962db863b657d9e63ef0a8426c10ee8ba28c7d04
Christian Haudum <christian.haudum@gmail.com>
2017-10-31 19:44:13 +0100

    show next greater suffix as soon as value exceeds 2^10

--- cstat/utils.py
+++ cstat/utils.py
@@ -6,7 +6,7 @@ BYTE_SIZES = ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y']
 
 def byte_size(value, suffix='b', k=1024):
     for unit in BYTE_SIZES:
-        if abs(value) < k * 10:
+        if abs(value) < k:
             break
         value /= k
     return '{0:.1f}{1}{2}'.format(value, unit, suffix)
